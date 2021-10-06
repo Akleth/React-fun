0d6969be32c29507c788ef8d1d693d420fb37407
Alex <a.k.leth002@gmail.com>
2021-10-06 14:21:52 +0200

    added functional buttons

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,20 @@
 const Home = () => {
+    // a click function which can't have any arguments passed to it:
+    const handleClick = () => {
+        console.log("hi")
+    }
+    const handleClickAgain = (name) => {
+        console.log("hi " + name)
+    }
+
+
+
     return ( 
     <div className="home"> 
-        <h2>Homepage</h2>
+        <h2>Homepage <br /></h2>
+        <b>These are buttons that say stuff in the console when you click them: <br/></b>
+        <button onClick={handleClick}>I can't take input arguments like name</button>
+        <button onClick={() => handleClickAgain("Alex")}>... but i can :)</button>
     </div> );
 }
  
