4ce6e1ef968073189e9c83bcaaccca4e61613845
Alex <a.k.leth002@gmail.com>
2021-10-06 13:52:36 +0200

    made very base webpage as a start to the portfolio website in react

---
+++ src/Navbar.js
@@ -0,0 +1,14 @@
+const Navbar = () => {
+    return ( 
+        <nav className="navbar">
+            <h1>Portfolio</h1>
+            <div className="links">
+                <a href="/">Home</a>
+                <a href="/catalouge">Catalouge</a>
+                <a href="/about">About</a>
+            </div>
+        </nav>
+     );
+}
+
+export default Navbar;
